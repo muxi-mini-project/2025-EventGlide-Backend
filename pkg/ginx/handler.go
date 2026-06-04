a52459de1001dc9cd808c3f7f20088a60b7fda5e
weiji6 <2954585173@qq.com>
2026-06-04 16:43:27 +0800

    fix:修复请求不带body导致的EOF问题

--- pkg/ginx/handler.go
+++ pkg/ginx/handler.go
@@ -2,10 +2,11 @@ package ginx
 
 import (
 	"errors"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/raiki02/EG/api/resp"
-	"net/http"
 )
 
 type userClaims struct{}
@@ -114,10 +115,12 @@ func bind(ctx *gin.Context, req any) (err error) {
 	if ctx.Request.Method == http.MethodGet {
 		err = ctx.ShouldBindQuery(req)
 	} else {
-		err = ctx.ShouldBind(req)
-	}
-	if err != nil {
-		return
+		if ctx.Request.ContentLength > 0 {
+			err = ctx.ShouldBind(req)
+			if err != nil {
+				return
+			}
+		}
 	}
 
 	if err = validateRequest(req); err != nil {
